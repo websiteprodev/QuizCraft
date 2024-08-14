860a17a2bb8e136ac47fb9d97c59aeddebd2cf3f
Daniel Stoyanov <stoyanov_d@hotmail.com>
2024-08-14 16:04:14 +0300

    upload project setup and firebase setup

---
+++ src/config/firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_kPnOX_kPgx1vE24O4-wHD6p5-BvFLPA",
+  authDomain: "quizcraftvps.firebaseapp.com",
+  projectId: "quizcraftvps",
+  storageBucket: "quizcraftvps.appspot.com",
+  messagingSenderId: "147367393909",
+  appId: "1:147367393909:web:43fb07b226dfc165a9163c",
+  databaseURL: "https://quizcraftvps-default-rtdb.europe-west1.firebasedatabase.app/"
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

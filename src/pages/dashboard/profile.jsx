5cf9635d50683b56ca2f3a6dc11cd3d981c13616
Daniel Stoyanov <stoyanov_d@hotmail.com>
2024-08-20 14:21:54 +0300

    add: link to register page in sign in page

--- src/pages/dashboard/profile.jsx
+++ src/pages/dashboard/profile.jsx
@@ -1,28 +1,15 @@
-import {
-  Card,
-  CardBody,
-  CardHeader,
-  CardFooter,
-  Avatar,
-  Typography,
-  Tabs,
-  TabsHeader,
-  Tab,
-  Switch,
-  Tooltip,
-  Button,
-} from "@material-tailwind/react";
-import {
-  HomeIcon,
-  ChatBubbleLeftEllipsisIcon,
-  Cog6ToothIcon,
-  PencilIcon,
-} from "@heroicons/react/24/solid";
+import { Card, CardBody, CardHeader, CardFooter, Avatar, Typography, Tabs, TabsHeader, Tab, Switch, Tooltip, Button, } from "@material-tailwind/react";
+import { HomeIcon, ChatBubbleLeftEllipsisIcon, Cog6ToothIcon, PencilIcon, } from "@heroicons/react/24/solid";
 import { Link } from "react-router-dom";
 import { ProfileInfoCard, MessageCard } from "@/widgets/cards";
 import { platformSettingsData, conversationsData, projectsData } from "@/data";
+import {getDoc, doc, updateDoc, query, where, getDocs, collection} from "firebase/firestore";
+import React, {useState, useEffect} from 'react';
+import { db } from "@/configs/firebase";
+import { useNavigate } from "react-router-dom";
 
-export function Profile() {
+export function Profile({username}) {
+
   return (
     <>
       <div className="relative mt-8 h-72 w-full overflow-hidden rounded-xl bg-[url('/img/background-image.png')] bg-cover	bg-center">

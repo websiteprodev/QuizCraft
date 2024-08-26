97ddc39f5bf2fe24406df668b718c14f74edc744
Daniel Stoyanov <stoyanov_d@hotmail.com>
2024-08-26 12:31:54 +0300

    fixed: Bug where site was not loading because of function declaration. Changed title name of the index.html

--- src/admin/AdminPanel.jsx
+++ src/admin/AdminPanel.jsx
@@ -1,16 +1,19 @@
-import React from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
-import AdminLayout from "./AdminLayout"; 
-import adminRoutes from "../adminRoutes"; 
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import AdminLayout from './AdminLayout';
+import adminRoutes from '../adminRoutes';
 
-export function AdminPanel() {
+export default function AdminPanel() {
     return (
         <AdminLayout>
             <Routes>
                 {adminRoutes.map(({ path, element }) => (
                     <Route key={path} path={path} element={element} />
                 ))}
-                <Route path="*" element={<Navigate to="/admin/user-management" replace />} />
+                <Route
+                    path="*"
+                    element={<Navigate to="/admin/user-management" replace />}
+                />
             </Routes>
         </AdminLayout>
     );
